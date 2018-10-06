21b2bfcdb18f8d8a694dff0643c1d1ef76ea13be
Alex Chen <auc39@psu.edu>
2018-10-06 16:51:00 -0400

    inserted python file

---
+++ DataCollection/historicalData.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as web
+import pmysql
+
+#from pandas_datareader.robinhood import RobinhoodQuoteReader, \
+#    RobinhoodHistoricalReader
+'''
+symbols = ["GOOG", "AMZN"]
+
+for i in symbols:
+    data = web.DataReader(i, 'robinhood')
+    data.to_csv(i+".csv", sep = ',')
+'''
+'''   
+f = web.DataReader('F', 'robinhood')
+Google = web.DataReader("GOOG", 'robinhood')
+Amazon = web.DataReader("AMZN", 'robinhood')
+'''
