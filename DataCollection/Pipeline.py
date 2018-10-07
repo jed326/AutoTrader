aefcf40a3f85fa130c1d9127e1ca8b57b1d652df
Jay Deng <jayd0104@gmail.com>
2018-10-06 20:12:36 -0400

    BigQuery Pipeline completed.

---
+++ DataCollection/Pipeline.py
@@ -0,0 +1,9 @@
+import insert, getDataFiveYear
+stocks = ['TWTR', 'YAHOY', 'YIPI', 'YOOIF', 'MOMO', 'GDDY', 'BBBY', 'INTC', 'PCAR', 'INTU', 'MDLZ', 'BIDU', 'LLTC', 'KLAC', 'GOOGL', 'PCLN', 'AMGN', 'CSCO', 'ISRG']
+
+for stock in stocks:
+    try:
+        getDataFiveYear.getData(stock)
+        insert.insertCSV(stock)
+    except:
+        print("unlucky")
--- DataCollection/insert.py
+++ DataCollection/insert.py
@@ -2,7 +2,7 @@
 from google.cloud import bigquery
 from google.cloud.bigquery import LoadJobConfig
 from google.cloud.bigquery import SchemaField
-import os
+import os, csv
 
 def query():
     client = bigquery.Client('autotrader-test-1')
@@ -16,8 +16,38 @@ def query():
     for row in rows:
         print(row.begins_at)
 
-def insertCSV():
-    
+"""
+symbol
+"""
+def insertCSV(stock):
+
+    client = bigquery.Client('autotrader-test-1')
+
+    SCHEMA = [
+        SchemaField('symbol', 'STRING', mode='required'),
+        SchemaField('date', 'DATE', mode='required'),
+        SchemaField('close', 'FLOAT', mode='required'),
+        SchemaField('high', 'FLOAT', mode='required'),
+        SchemaField('low', 'FLOAT', mode='required'),
+        SchemaField('open', 'FLOAT', mode='required'),
+        SchemaField('volume', 'INTEGER', mode='required'),
+    ]
+
+    table_ref = client.dataset('HistoricalPrices').table(stock)
+
+    load_config = LoadJobConfig()
+    load_config.skip_leading_rows = 1
+    load_config.schema = SCHEMA
+
+    with open('%s.csv' % stock, 'rb') as readable:
+        r = csv.reader(readable, delimiter=',')
+        # for row in r:
+        #     print(r)
+        client.load_table_from_file(
+            readable, table_ref, job_config=load_config)
+
+if __name__ == "__main__":
+    insertCSV("GOOG")
 
 # dataset_id = 'autotrader-test-1:HistoricalPrices'  # replace with your dataset ID
 # # For this sample, the table must already exist and have a defined schema
