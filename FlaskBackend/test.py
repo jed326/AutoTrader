091f442c6eb67228ae80e635b56240491372a827
jefferycao1 <jefferycao2016@gmail.com>
2018-10-07 04:40:05 -0700

    added final page

--- FlaskBackend/test.py
+++ FlaskBackend/test.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from Robinhood import Robinhood
 import sys
+import datetime
 sys.path.append("../")
 from DataCollection import database
 
@@ -23,7 +24,12 @@ def login():
 
 @app.route('/yeet')
 def yeet():
-    stocks = database.getAllStocks()
+    now = datetime.datetime.now()
+
+    date = str(now.year) + '-' + str(now.month) + '-'  + str(now.day)
+    date = '2018-10-05'
+    print(date)
+    stocks = database.getPrices(date)
     print(stocks)
     return render_template('stockchooser.html', stocks = stocks)
 
