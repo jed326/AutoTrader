fe9292c97dcce8ef15a99806f4ac05f043d524a0
Jay Deng <jayd0104@gmail.com>
2018-10-06 21:22:27 -0400

    touchups

--- DataCollection/database.py
+++ DataCollection/database.py
@@ -7,13 +7,15 @@ import os, csv
 def query(stock):
     client = bigquery.Client('autotrader-test-1')
 
-    QUERY = ('SELECT symbol FROM `autotrader-test-1.HistoricalPrices.%s` LIMIT 1000' % stock)
+    QUERY = ('SELECT * FROM `autotrader-test-1.HistoricalPrices.%s` LIMIT 1000' % stock)
 
     query_job = client.query(QUERY)
     rows = query_job.result()
 
-    for row in rows:
-        print(row.symbol)
+    # for row in rows:
+    #     print(row['high'])
+
+    return rows
 
 def insertCSV(stock):
 
@@ -42,6 +44,11 @@ def insertCSV(stock):
         client.load_table_from_file(
             readable, table_ref, job_config=load_config)
 
+def insertRow(stock):
+    pass
+
 if __name__ == "__main__":
     #insertCSV("GOOG")
-    query("TWTR")
+    r = query("TWTR")
+    for x in r:
+        print(x['close'])
---
+++ TradingEngine.py
@@ -0,0 +1,10 @@
+from DataCollection import database
+
+#Run every MWF at market open
+
+#insert yesterday's closing data into database
+
+    #get all stocks from dataset
+    #update all data
+
+#run model based on update dataset
